9d2e10e7efffe427dc9d65c813a2ce8d25dd5053
Garrett <132799173+TheMoose477@users.noreply.github.com>
2023-09-11 20:49:18 -0600

    Add files via upload

---
+++ shutdown.py
@@ -0,0 +1,13 @@
+import pyautogui
+import os
+
+print(pyautogui.size())
+
+
+
+pyautogui.moveTo(25, 1065, duration=1)
+pyautogui.leftClick()
+pyautogui.moveTo(25, 1020, duration=0.25)
+pyautogui.leftClick()
+pyautogui.moveTo(75, 940, duration=0.25)
+pyautogui.leftClick()
